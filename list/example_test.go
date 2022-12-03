7f0f13a15521ebaad38312a07c141dfbed9260a0
ICHINOSE Shogo <shogo82148@gmail.com>
2022-12-03 10:32:25 +0900

    add example tet for Zip2

---
+++ list/example_test.go
@@ -0,0 +1,27 @@
+package list_test
+
+import (
+	"fmt"
+
+	"github.com/shogo82148/hi/list"
+)
+
+func ExampleZip2() {
+	var l1 list.List[int]
+	var l2 list.List[string]
+	l1.PushBack(1)
+	l1.PushBack(2)
+	l1.PushBack(3)
+	l2.PushBack("one")
+	l2.PushBack("two")
+	l2.PushBack("three")
+
+	l := list.Zip2(l1, l2)
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Println(e.Value)
+	}
+	// Output:
+	// (1, one)
+	// (2, two)
+	// (3, three)
+}
