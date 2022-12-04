d03fbac53c546ef68119931d9aa3058cee6144d2
ICHINOSE Shogo <shogo82148@gmail.com>
2022-12-04 22:20:05 +0900

    add type aliases

---
+++ sync/sync.go
@@ -0,0 +1,15 @@
+package sync
+
+import "sync"
+
+type Cond = sync.Cond
+
+type Locker = sync.Locker
+
+type Mutex = sync.Mutex
+
+type Once = sync.Once
+
+type RWMutex = sync.RWMutex
+
+type WaitGroup = sync.WaitGroup
