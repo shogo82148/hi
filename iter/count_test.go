4d1d9add87ea1c02f7f434c43802e9748cda4fd9
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-10 01:58:03 +0900

    implement iter.Count

---
+++ iter/count_test.go
@@ -0,0 +1,20 @@
+package iter
+
+import (
+	"testing"
+	"reflect"
+)
+
+func TestCount(t *testing.T) {
+	got := make([]int, 0)
+	for v := range Count(0, 2) {
+		got = append(got, v)
+		if len(got) >= 5 {
+			break
+		}
+	}
+	want := []int{0, 2, 4, 6, 8}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
