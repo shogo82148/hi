8e9389f2a5c951166806436ddab66ee10f3c057e
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-16 22:35:10 +0900

    implement Result.FilterFalse

--- result/example_test.go
+++ result/example_test.go
@@ -16,3 +16,31 @@ func ExampleUnzip2() {
 	// 1
 	// one
 }
+
+func ExampleFilter() {
+	filter := func(v int) bool { return v > 20 }
+
+	v := result.OK(10).Filter(filter)
+	fmt.Println(v.Err())
+
+	w := result.OK(30).Filter(filter)
+	fmt.Println(w.Err())
+
+	// Output:
+	// result: filtered
+	// <nil>
+}
+
+func ExampleFilterFalse() {
+	filter := func(v int) bool { return v > 20 }
+
+	v := result.OK(10).FilterFalse(filter)
+	fmt.Println(v.Err())
+
+	w := result.OK(30).FilterFalse(filter)
+	fmt.Println(w.Err())
+
+	// Output:
+	// <nil>
+	// result: filtered
+}
