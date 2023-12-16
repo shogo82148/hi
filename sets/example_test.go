8aed24093642f59641c36cf6e0ab00583d321d77
ICHINOSE Shogo <shogo82148@gmail.com>
2023-12-16 22:39:32 +0900

    implement Set.FilterFalse

--- sets/example_test.go
+++ sets/example_test.go
@@ -6,6 +6,31 @@ import (
 	"github.com/shogo82148/hi/sets"
 )
 
+func ExampleSet_Filter() {
+	s := sets.New(1, 2, 3, 4, 5)
+	u := s.Filter(func(v int) bool { return v > 3 })
+
+	for v := range u {
+		fmt.Println(v)
+	}
+	// Unordered output:
+	// 4
+	// 5
+}
+
+func ExampleSet_FilterFalse() {
+	s := sets.New(1, 2, 3, 4, 5)
+	u := s.FilterFalse(func(v int) bool { return v > 3 })
+
+	for v := range u {
+		fmt.Println(v)
+	}
+	// Unordered output:
+	// 1
+	// 2
+	// 3
+}
+
 func ExampleSet_Count() {
 	s := sets.New(1, 2, 3, 4, 5)
 	cnt := s.Count(func(v int) bool { return v > 3 })
